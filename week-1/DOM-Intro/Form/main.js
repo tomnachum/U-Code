18103dc8e49c3887ae1c4b2964fdf8d1af7169bb
tom-nachum <tom.nachum@gmail.com>
2022-08-18 16:58:53 +0300

    Form extension completed

--- week-1/DOM-Intro/Form/main.js
+++ week-1/DOM-Intro/Form/main.js
@@ -3,7 +3,6 @@ function validate() {
   const salary = document.getElementById("desired-salary").value;
   const birthday = document.getElementById("birthday").value;
   const phone = document.getElementById("phone").value;
-  console.log(birthday);
   if (name.length <= 2) {
     createError("Name must be longer than 2 characters");
   }
@@ -15,12 +14,21 @@ function validate() {
   }
   if (phone.length !== 10) {
     createError("Phone must be 10 digits long");
+  } else {
+    welcomeUser(name);
   }
+}
+
+function welcomeUser(name) {
+  document.getElementsByClassName("container")[0].style.display = "none";
+  let welcome = document.createElement("h1");
+  welcome.innerHTML = `Hi ${name}, welcome!`;
+  document.body.appendChild(welcome);
 }
 
 function createError(text) {
   let error = document.createElement("p");
   error.style.color = "red";
   error.innerHTML = text;
-  document.body.appendChild(error);
+  document.getElementsByClassName("errors")[0].appendChild(error);
 }
