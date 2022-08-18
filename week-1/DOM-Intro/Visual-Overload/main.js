40748cf3dd2483a7ec2ae10175064999736b1449
tom-nachum <tom.nachum@gmail.com>
2022-08-18 15:57:24 +0300

    Finished Visual Overload Exercise

--- week-1/DOM-Intro/Visual-Overload/main.js
+++ week-1/DOM-Intro/Visual-Overload/main.js
@@ -1,7 +1,10 @@
 //add boxes
 for (let i = 0; i < 4; i++) {
   let box = document.createElement("div");
-  box.style.height = "100px";
-  box.style.border = "solid";
+  box.setAttribute("class", "box");
+  box.onclick = function () {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    box.style.backgroundColor = "#" + randomColor;
+  };
   document.getElementById("container").appendChild(box);
 }
