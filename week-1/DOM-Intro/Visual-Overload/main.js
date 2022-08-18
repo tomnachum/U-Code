d479d506751787547e01432c63b2a3667b34d8a4
tom-nachum <tom.nachum@gmail.com>
2022-08-18 15:50:34 +0300

    Created 4 boxes using js

---
+++ week-1/DOM-Intro/Visual-Overload/main.js
@@ -0,0 +1,7 @@
+//add boxes
+for (let i = 0; i < 4; i++) {
+  let box = document.createElement("div");
+  box.style.height = "100px";
+  box.style.border = "solid";
+  document.getElementById("container").appendChild(box);
+}
