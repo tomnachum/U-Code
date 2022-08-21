ee119c8ad70c2e0221e1e0489750459bd2e887fa
tom-nachum <tom.nachum@gmail.com>
2022-08-21 16:51:22 +0300

    commented test values

--- week-2/Tweeter-Logic-Module/main.js
+++ week-2/Tweeter-Logic-Module/main.js
@@ -1,30 +1,35 @@
 function Tweeter() {
-  let _postsIdCounter = 3 - 1; //TODO: change to 0
-  let _commentsIdCounter = 7 - 1; //TODO: change to 0
+  //   let _postsIdCounter = 3 - 1;
+  //   let _commentsIdCounter = 7 - 1;
 
-  let _postsData = [
-    {
-      text: "First post!",
-      id: "p1",
-      comments: [
-        { id: "c1", text: "First comment on first post!" },
-        { id: "c2", text: "Second comment on first post!!" },
-        { id: "c3", text: "Third comment on first post!!!" },
-      ],
-    },
-    {
-      text: "Aw man, I wanted to be first",
-      id: "p2",
-      comments: [
-        {
-          id: "c4",
-          text: "Don't wory second poster, you'll be first one day.",
-        },
-        { id: "c5", text: "Yeah, believe in yourself!" },
-        { id: "c6", text: "Haha second place what a joke." },
-      ],
-    },
-  ];
+  //   let _postsData = [
+  //     {
+  //       text: "First post!",
+  //       id: "p1",
+  //       comments: [
+  //         { id: "c1", text: "First comment on first post!" },
+  //         { id: "c2", text: "Second comment on first post!!" },
+  //         { id: "c3", text: "Third comment on first post!!!" },
+  //       ],
+  //     },
+  //     {
+  //       text: "Aw man, I wanted to be first",
+  //       id: "p2",
+  //       comments: [
+  //         {
+  //           id: "c4",
+  //           text: "Don't wory second poster, you'll be first one day.",
+  //         },
+  //         { id: "c5", text: "Yeah, believe in yourself!" },
+  //         { id: "c6", text: "Haha second place what a joke." },
+  //       ],
+  //     },
+  //   ];
+
+  let _postsIdCounter = 0;
+  let _commentsIdCounter = 0;
+
+  let _postsData = [];
 
   const getPosts = () => _postsData;
 
