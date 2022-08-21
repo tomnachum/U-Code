5a5e047ddf29ddb4f8d56dfda79fb3e91e599fac
tom-nachum <tom.nachum@gmail.com>
2022-08-21 14:13:12 +0300

    finished ex7

---
+++ week-2/callbacks-and-arrow-functions/Ex7/main.js
@@ -0,0 +1,11 @@
+const explode = (lightFunc, soundFunc, sound) => {
+  lightFunc();
+  soundFunc(sound);
+};
+
+const shineLight = () =>
+  (document.getElementById("box").style.backgroundColor = "yellow");
+
+const makeSound = (sound) => alert(sound);
+
+explode(shineLight, makeSound, "BOOM");
