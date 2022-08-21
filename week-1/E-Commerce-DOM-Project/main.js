2664f0fa5af8abf6f5b4a17201bfcc0f5c5c458a
tom-nachum <tom.nachum@gmail.com>
2022-08-21 17:22:15 +0300

    added HTML to e-commerence project with nav-bar and buttons

---
+++ week-1/E-Commerce-DOM-Project/main.js
@@ -0,0 +1,23 @@
+let cart = [];
+let products = [
+  {
+    name: "car",
+    price: 50000,
+    img: "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200",
+  },
+  {
+    name: "watch",
+    price: 150,
+    img: "https://m.media-amazon.com/images/I/81nC4u9eYfL._UY445_.jpg",
+  },
+  {
+    name: "pc",
+    price: 5000,
+    img: "https://m.media-amazon.com/images/I/819XYUimTuL._AC_SL1500_.jpg",
+  },
+  {
+    name: "bottle",
+    price: 50,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKqTFR3_ZH3GzXgzCL5liu4WvZoO3j6Y6G4UOBg4k&s",
+  },
+];
