c6bc194348e63d5fa1b19947019d8d9729df8347
tom-nachum <tom.nachum@gmail.com>
2022-08-22 15:57:23 +0300

    finished exercise 1

---
+++ week-2/jQuery/Ex1/main.js
@@ -0,0 +1,8 @@
+$("body").append(`<input placeholder="Human Name">`);
+$("body").append(`<button>Add Human</button>`);
+
+$("body").on("click", "button", function () {
+  const input = $("input").val();
+  $("ul").append(`<li>${input}</li>`);
+  $("input").val("");
+});
