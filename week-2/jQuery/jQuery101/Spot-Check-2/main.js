b1a8fa3509ab2f787c20212da94060779f7af953
tom-nachum <tom.nachum@gmail.com>
2022-08-22 11:04:24 +0300

    finished spot check 2

---
+++ week-2/jQuery/jQuery101/Spot-Check-2/main.js
@@ -0,0 +1,8 @@
+$("h1").css("color", "blue");
+$(".red-div").css("color", "red");
+$("ul").children("li").eq(0).css("color", "green");
+$("ul").children("li").eq(1).css("color", "pink");
+//also works
+// $("li:first-child").css("background-color", "green");
+// $("li:last-child").css("background-color", "pink");
+$("#brown-div").css("color", "brown");
