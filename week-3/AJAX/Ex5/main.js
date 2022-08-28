4f2a85eb4aa57381e0576db9c44bc850bfeafa33
tom-nachum <tom.nachum@gmail.com>
2022-08-28 13:06:56 +0300

    added iframe element instead of appending to div

--- week-3/AJAX/Ex5/main.js
+++ week-3/AJAX/Ex5/main.js
@@ -3,9 +3,7 @@ function getRequest(gif) {
     `http://api.giphy.com/v1/gifs/search?q=${gif}&api_key=mrg2SLPCUEpTXb1RWgIZVPktllRdJX34&limit=5`,
     function (data) {
       const firstImgUrl = data.data[0].embed_url;
-      $("#gif-generator").append(`
-          <iframe src=${firstImgUrl}>
-      `);
+      $("iframe").attr("src", firstImgUrl);
     }
   );
 }
