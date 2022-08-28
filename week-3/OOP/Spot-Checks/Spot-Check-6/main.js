6fce30216003152b19d27fce555811eebe74f8c0
tom-nachum <tom.nachum@gmail.com>
2022-08-28 16:56:59 +0300

    finished spo check 6

---
+++ week-3/OOP/Spot-Checks/Spot-Check-6/main.js
@@ -0,0 +1,37 @@
+class Vehicle {
+  static carsSold = 0;
+
+  constructor(x, y, speed, fuel) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this._fuel = fuel;
+  }
+
+  getInfo() {
+    console.log("Cars are great");
+  }
+
+  static calculateMoney() {
+    return `Made ${this.carsSold * 30000} dollars`;
+  }
+
+  get fuel() {
+    return this._fuel;
+  }
+
+  set fuel(fuel) {
+    if (fuel >= 0 && fuel <= 150) {
+      this._fuel = fuel;
+    }
+  }
+}
+
+let v = new Vehicle(1, 2, 3, 4);
+console.log(v.fuel);
+v.fuel = 200;
+console.log(v.fuel);
+v.fuel = -50;
+console.log(v.fuel);
+v.fuel = 130;
+console.log(v.fuel);
