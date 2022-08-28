ffaad678324a5b5b6c62917f2cb20bfd00222bc9
tom-nachum <tom.nachum@gmail.com>
2022-08-28 15:32:34 +0300

    finished ex7

--- week-3/ES6-Array-Methods/Exercises/Ex7/main.js
+++ week-3/ES6-Array-Methods/Exercises/Ex7/main.js
@@ -230,3 +230,11 @@ const users = [
     },
   },
 ];
+
+function printUser(user) {
+  console.log(
+    `${user.name} lives in ${user.address.city}, and owns the company ${user.company.name}`
+  );
+}
+
+users.forEach(printUser);
