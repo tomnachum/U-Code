62757f2e8301ba0c01ee60f39e57424d3d65cd9d
tom-nachum <tom.nachum@gmail.com>
2022-08-28 14:45:46 +0300

    changed order of folders

---
+++ week-3/ES6-Array-Methods/map/main.js
@@ -0,0 +1,8 @@
+let messagesFromDad = [
+  "HI HONEY",
+  "HOW WAS SCHOOL??",
+  "DID YOU EAT TODAY?",
+  "I CAN'T FIND THE REMOTE CONTROL",
+];
+
+const lowerCased = messagesFromDad.map(m => m.toLowerCase());
