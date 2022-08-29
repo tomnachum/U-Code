c21faf7fe2571f0379e5d0142e0a82763f1b4574
tom-nachum <tom.nachum@gmail.com>
2022-08-29 18:39:02 +0300

    finished nullish operator

---
+++ week-3/Destructuring&Spread&Nullish/Null-Coalescing-Exercises/Employees-Filter/main.js
@@ -0,0 +1,17 @@
+let employeesArr = [
+  { name: "Joey", id: 1, age: 26 },
+  { name: "Lily", id: null, age: 24 },
+  { name: "Alice", id: 7, age: null },
+  { name: "Sam", id: 8, age: 24 },
+  { name: "Ray", id: null, age: null },
+];
+
+//assuming id and age cannot be strings
+employeesArr.forEach(e => {
+  if (
+    typeof (e.id ?? e.name) === "string" ||
+    typeof (e.age ?? e.name) === "string"
+  ) {
+    console.log(e.name);
+  }
+});
