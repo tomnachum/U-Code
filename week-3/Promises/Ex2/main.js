acd02689e302a19faedf0ca8e6a4c5369c3efae7
tom-nachum <tom.nachum@gmail.com>
2022-08-31 12:08:32 +0300

    added render function

--- week-3/Promises/Ex2/main.js
+++ week-3/Promises/Ex2/main.js
@@ -1,9 +1,5 @@
 $.get("https://random-word-api.herokuapp.com/word").then(function (words) {
   const word = words[0];
-  $("body").append(`
-    <h2>${word}</h2>
-  `);
-  console.log(`random word: ${word}`);
   const titlePromise = $.get(
     `https://www.googleapis.com/books/v1/volumes?q=intitle:${word}`
   );
@@ -12,10 +8,17 @@ $.get("https://random-word-api.herokuapp.com/word").then(function (words) {
   );
   Promise.all([titlePromise, imgPromise]).then(function (responses) {
     const title = responses[0].items[0].volumeInfo.title;
-    const img = responses[1].data[0].embed_url;
-    $("body").append(`
-        <h3>${title}</h3>
-        <iframe src=${img} style="{ width: '100%', height: 'auto' }" frameborder="0"></iframe>
-    `);
+    const imgUrl = responses[1].data[0].embed_url;
+    render(word, title, imgUrl);
   });
 });
+
+function render(word, title, imgUrl) {
+  $("body").append(`
+    <h2>Random word:</h2>
+    <p>${word}</p>
+    <h3>Title based on that word:</h3>
+    <p>${title}</p>
+    <iframe src=${imgUrl} style="{ width: '100%', height: 'auto' }" frameborder="0"></iframe>
+  `);
+}
