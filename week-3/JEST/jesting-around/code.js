3f60c676d901e9e3c9c8adee455eb30f0b45ef33
tom-nachum <tom.nachum@gmail.com>
2022-09-01 12:36:35 +0300

    finished testing examples

--- week-3/JEST/jesting-around/code.js
+++ week-3/JEST/jesting-around/code.js
@@ -2,6 +2,14 @@ class Calculator {
   add(a, b) {
     return a + b;
   }
+
+  calculateHyp(a, b) {
+    return Math.sqrt(a ** 2 + b ** 2);
+  }
 }
 
-module.exports = Calculator;
+function clearLowPriority(arr) {
+  return arr.filter(o => o.priority === "HIGH");
+}
+
+module.exports = { Calculator, clearLowPriority };
--- week-3/JEST/jesting-around/code.test.js
+++ week-3/JEST/jesting-around/code.test.js
@@ -1,7 +1,24 @@
-const Calculator = require("./code");
-let calculator = new Calculator();
+const codeModule = require("./code.js");
+const calculator = new codeModule.Calculator();
+const clearLowPriority = codeModule.clearLowPriority;
 
 test("add should return sum of a + b", () => {
   let sum = calculator.add(1, 2);
   expect(sum).toBe(3);
+});
+
+test("calculateHyp should return the square root of the sum of the numbers’ squares", () => {
+  let pitagoras = calculator.calculateHyp(3, 4);
+  expect(pitagoras).toBe(5);
+});
+
+test("clearLowPriority should delete objects with LOW priority", () => {
+  const mockData = [
+    { test: "hi", priority: "LOW" },
+    { test: "hi", priority: "HIGH" },
+  ];
+  const clearedData = clearLowPriority(mockData);
+  clearedData.forEach(element => {
+    expect(element.priority).toBe("HIGH");
+  });
 });
