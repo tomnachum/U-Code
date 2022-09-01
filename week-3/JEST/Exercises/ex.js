d831fd107b12f8f1b7f74987b4655d1b36925e07
tom-nachum <tom.nachum@gmail.com>
2022-09-01 14:31:41 +0300

    added tests to ex1-ex3

--- week-3/JEST/Exercises/ex.js
+++ week-3/JEST/Exercises/ex.js
@@ -1,3 +1,23 @@
-class Exercise {}
+class Exercise {
+  //should return true if n is even, false otherwise
+  isEven(n) {
+    return n % 2 == 0 ? true : false;
+  }
+  //should remove at least one element from the array `arr`
+  removeAtLeastOne(arr) {
+    let numItemsToRemove = Math.floor(Math.random() * (arr.length - 1)) + 1;
+    arr.splice(0, numItemsToRemove);
+    return arr;
+  }
+
+  //should return a clean string without these symbols: "!", "#", ".", ",", "'"
+  simplify(str) {
+    let symbols = ["!", "#", ".", ",", "'"];
+    return str
+      .split("")
+      .filter(c => symbols.indexOf(c) == -1)
+      .join("");
+  }
+}
 
 module.exports = Exercise;
