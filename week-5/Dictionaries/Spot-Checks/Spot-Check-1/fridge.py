0c99b1533b0d0a35d397dc57345aeea27be4dcd9
tom-nachum <tom.nachum@gmail.com>
2022-09-11 10:39:30 +0300

    finished spot check 1

---
+++ week-5/Dictionaries/Spot-Checks/Spot-Check-1/fridge.py
@@ -0,0 +1,9 @@
+fridge = {"milk":1,
+"bananas":4,
+"juice":2}
+
+milkCounter = fridge["milk"]
+if milkCounter==1:
+    print("There is one bottle of milk left")
+else:
+    print(f"There are {milkCounter} bottles of milk left")
