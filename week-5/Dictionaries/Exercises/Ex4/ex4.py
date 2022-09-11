d95287ebc9c68e6e9f692120851e7323245e2558
tom-nachum <tom.nachum@gmail.com>
2022-09-11 11:16:11 +0300

    finished ex4

---
+++ week-5/Dictionaries/Exercises/Ex4/ex4.py
@@ -0,0 +1,21 @@
+tomato = "Tomato Soup"
+onion = "Onion Soup"
+carrot = "Carrot Soup"
+
+user_preferences = {
+    "Sarah": tomato,
+    "Sheila": carrot,
+    "Fernando": tomato,
+    "Jovan": onion,
+    "Simona": carrot
+}
+
+soup_recipes = {
+    tomato: "Get a bunch of tomatoes, cut them up, and throw them in boiling water",
+    onion: "Be prepared to cry",
+    carrot: "Find a rabbit, ask him how to make a carrot soup"
+}
+
+user = "Jovan"
+
+print(soup_recipes[user_preferences[user]])
