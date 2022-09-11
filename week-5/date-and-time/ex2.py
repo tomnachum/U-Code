59e1a1c510eeb74e269e0d13f146f495190cfca5
tom-nachum <tom.nachum@gmail.com>
2022-09-11 18:10:10 +0300

    finished ex2

---
+++ week-5/date-and-time/ex2.py
@@ -0,0 +1,5 @@
+import datetime
+
+print(f"time: {datetime.datetime.today().strftime('%H:%M:%S')}")
+print(f"current2: {datetime.datetime.today().strftime('%m/%d/%Y/, %H.%M.%S')}")
+print(f"current2: {datetime.datetime.today().strftime('%d/%m/%Y/, %H.%M.%S')}")
