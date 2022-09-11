e4f9daa7a7ce5cc7091815ed787673e8592a22b3
tom-nachum <tom.nachum@gmail.com>
2022-09-11 11:08:13 +0300

    finished ex1

---
+++ week-5/Dictionaries/Exercises/Ex1/ex1.py
@@ -0,0 +1,19 @@
+state = "Italy"
+
+heads_of_states = {
+    "Italy": {
+        "president": "Sergio Mattarella",
+        "prime minister": "Giuseppe Conte"
+    },
+    "India": {
+        "president": "Ram Nath Kovind",
+        "prime minister": "Narendra Modi"
+    },
+    "France": {
+        "president": "Emmanuel Macron",
+        "prime minister": "Edouard Philippe"
+    }
+}
+
+print(f"The president of {state} is {heads_of_states[state]['president']} and the prime minister is {heads_of_states[state]['prime minister']}")
+
