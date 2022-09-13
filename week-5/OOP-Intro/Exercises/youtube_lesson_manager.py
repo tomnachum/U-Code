353be3a68a66953315d7d9254bcfb8542d3d976e
tom-nachum <tom.nachum@gmail.com>
2022-09-13 10:55:28 +0300

    finished ex3

--- week-5/OOP-Intro/Exercises/youtube_lesson_manager.py
+++ week-5/OOP-Intro/Exercises/youtube_lesson_manager.py
@@ -4,3 +4,24 @@ class YouTubeLessonManager:
 
     def save(self, name, url):
         self.lessons[name] = url
+
+    def get(self, lesson_name):
+        return self.lessons[lesson_name]
+
+
+# Tests
+lesson_manager = YouTubeLessonManager()
+print(lesson_manager.lessons)
+
+lesson_manager = YouTubeLessonManager()
+lesson_manager.save("For-Loops", "https://www.youtube.com/watch?v=OnDr4J2UXSA")
+print(
+    lesson_manager.lessons
+)  # outputs: {"For-Loops": "https://www.youtube.com/watch?v=OnDr4J2UXSA"}
+print(
+    lesson_manager.lessons["For-Loops"]
+)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
+
+print(
+    lesson_manager.get("For-Loops")
+)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
