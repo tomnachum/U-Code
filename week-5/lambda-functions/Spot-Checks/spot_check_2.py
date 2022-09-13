f8b56fcc37dc5b8fda9ca41a89925da174ea06b7
tom-nachum <tom.nachum@gmail.com>
2022-09-13 15:31:49 +0300

    finished spot checks

---
+++ week-5/lambda-functions/Spot-Checks/spot_check_2.py
@@ -0,0 +1,8 @@
+menu = [
+    {"name": "Beef stew", "vegetarian": False},
+    {"name": "Beef sandwhich", "vegetarian": False},
+    {"name": "Carrot on a stick", "vegetarian": True},
+    {"name": "Beef eggroll", "vegetarian": False},
+]
+
+print(list(filter(lambda dish: dish["vegetarian"], menu)))
---
+++ week-5/lambda-functions/Spot-Checks/spot_check_3.py
@@ -0,0 +1,3 @@
+things = ["tree", "leaves", "green", "letter", "envelope", "cost", "money", "check"]
+
+print(list(map(lambda s: len(s), things)))
