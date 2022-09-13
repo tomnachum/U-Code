51f8e11d25548aa9173d09b5a9b4c267652a649e
tom-nachum <tom.nachum@gmail.com>
2022-09-13 10:39:20 +0300

    stated one liner exercises

---
+++ week-5/One-liner/exercises.py
@@ -0,0 +1,45 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+
+def reverse_word(sentence):
+    return " ".join([word[::-1] for word in sentence.split(" ")])
+
+
+# print(reverse_word("hello world"))
+# print(reverse_word("my name is Tom 3435"))
+
+
+def swap():
+    x = 2
+    y = 3
+    x, y = y, x
+    print(x, y)
+
+
+def sum_even_items(arr):
+    return sum(arr[1::2])
+
+
+# print(sum_even_items([1, 2, 1, 3, 1]))
+
+
+def duplicate(arr):
+    return arr + arr
+
+
+# print(duplicate([1, 2, 3]))
+
+
+def change_spaces(s):
+    return s.replace(" ", "@")
+
+
+# print(change_spaces("You got it"))
+
+
+def number_of_spaces(s):
+    pass
+
+
+print(number_of_spaces("my name is what"))
