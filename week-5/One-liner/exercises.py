bc19a75e1516b14c547975e2ff0d729f4d7437cf
tom-nachum <tom.nachum@gmail.com>
2022-09-13 14:53:56 +0300

    finished one liner exercises

--- week-5/One-liner/exercises.py
+++ week-5/One-liner/exercises.py
@@ -6,10 +6,6 @@ def reverse_word(sentence):
     return " ".join([word[::-1] for word in sentence.split(" ")])
 
 
-# print(reverse_word("hello world"))
-# print(reverse_word("my name is Tom 3435"))
-
-
 def swap():
     x = 2
     y = 3
@@ -21,32 +17,36 @@ def sum_even_items(arr):
     return sum(arr[1::2])
 
 
-# print(sum_even_items([1, 2, 1, 3, 1]))
-
-
 def duplicate(arr):
     return arr + arr
-
-
-# print(duplicate([1, 2, 3]))
 
 
 def change_spaces(s):
     return s.replace(" ", "@")
 
 
-# print(change_spaces("You got it"))
-
-
 def number_of_spaces(s):
     return len([c for c in s if c == " "])
-
-
-# print(number_of_spaces("my name is what     "))
 
 
 def number_of_all_spaces(s):
     return len([c for c in s if c.isspace()])
 
 
-print(number_of_spaces("my name is what     "))
+def largest_lexicographically(s):
+    return max(s.replace(".", "").split(" "))
+
+
+def longest_word(s):
+    return max([len(word) for word in s.replace(".", "").split(" ")])
+
+
+def reverse_senrence(s):
+    return s[::-1]
+
+
+def reverse_word_order(s):
+    return " ".join((s.split(" ")[::-1]))
+
+
+print(reverse_word_order("hello dear world"))
