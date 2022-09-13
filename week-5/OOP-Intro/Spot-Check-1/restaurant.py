f8c5453098801a735d89930903453031ed15a4fb
tom-nachum <tom.nachum@gmail.com>
2022-09-13 10:48:13 +0300

    finished spot check 2

--- week-5/OOP-Intro/Spot-Check-1/restaurant.py
+++ week-5/OOP-Intro/Spot-Check-1/restaurant.py
@@ -9,9 +9,14 @@ class Restaurant:
         self.ratings = ratings
         self.is_vegitarian = is_vegitarian
 
+    def get_menu(self):
+        print("We have vegtables" if self.is_vegitarian else "We have meat")
+
 
 r1 = Restaurant("Zoozaazo", 4, False)
 r2 = Restaurant("Top La Pompei", 3, False)
 r3 = Restaurant("El Viego", 5, True)
 
 print(r3.ratings)
+r3.get_menu()
+r2.get_menu()
