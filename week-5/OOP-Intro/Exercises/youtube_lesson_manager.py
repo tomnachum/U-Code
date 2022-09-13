c4f793c5387cc68baeba3b0aa1184e31999e0155
tom-nachum <tom.nachum@gmail.com>
2022-09-13 11:44:33 +0300

    finished extenstion 2

--- week-5/OOP-Intro/Exercises/youtube_lesson_manager.py
+++ week-5/OOP-Intro/Exercises/youtube_lesson_manager.py
@@ -5,16 +5,19 @@ class YouTubeLessonManager:
     def __init__(self) -> None:
         self._lessons = {}
         self._youtube_url = "https://www.youtube.com/watch?v="
+        self.counter = {}
 
     def save(self, name, url):
         self._lessons[self._edit_name(name)] = url[-11:]
+        self.counter[self._edit_name(name)] = 0
 
     def get(self, lesson_name):
-        return [
-            self._youtube_url + self._lessons[name]
-            for name in self._lessons
-            if self._edit_name(lesson_name) in name
-        ]
+        lessons = []
+        for name, postfix in self._lessons.items():
+            if self._edit_name(lesson_name) in name:
+                lessons.append(self._youtube_url + postfix)
+                self.counter[self._edit_name(name)] += 1
+        return lessons
 
     def _edit_name(self, name):
         return (
@@ -23,10 +26,20 @@ class YouTubeLessonManager:
             .replace(" ", "")
         )
 
+    def get_counts(self, name):
+        return self.counter[self._edit_name(name)]
+
 
 lesson_manager = YouTubeLessonManager()
+lesson_manager.save("Dictionaries", "https://www.youtube.com/watch?v=OnDr4J2UXSA")
 lesson_manager.save("For-Loops", "https://www.youtube.com/watch?v=OnDr4J2UXSA")
-print(lesson_manager._lessons["forloops"])  # outputs: OnDr4J2UXSA
-print(
-    lesson_manager.get("For-Loops")
-)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
+lesson_manager.get("For-Loops")
+lesson_manager.get("For-Loops")
+lesson_manager.get("Dictionaries")
+lesson_manager.get("For-Loops")
+lesson_manager.get("Dictionaries")
+print(lesson_manager.get_counts("For-Loops"))  # outputs: 3
+print(lesson_manager.get_counts("Dictionaries"))  # outputs: 2
+lesson_manager.get("For-Loops")
+lesson_manager.get("Loops")
+print(lesson_manager.get_counts("For-Loops"))
