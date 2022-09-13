a1068e4b92d69b3ff4ae59e1e9ca872184ea0d6a
tom-nachum <tom.nachum@gmail.com>
2022-09-13 11:35:02 +0300

    finished bonus & extension 1

--- week-5/OOP-Intro/Exercises/youtube_lesson_manager.py
+++ week-5/OOP-Intro/Exercises/youtube_lesson_manager.py
@@ -2,19 +2,20 @@ import string
 
 
 class YouTubeLessonManager:
-    # ex1
     def __init__(self) -> None:
-        self.lessons = {}
+        self._lessons = {}
+        self._youtube_url = "https://www.youtube.com/watch?v="
 
-    # ex2
     def save(self, name, url):
-        self.lessons[self._edit_name(name)] = url
+        self._lessons[self._edit_name(name)] = url[-11:]
 
-    # ex3
     def get(self, lesson_name):
-        return self.lessons[self._edit_name(lesson_name)]
+        return [
+            self._youtube_url + self._lessons[name]
+            for name in self._lessons
+            if self._edit_name(lesson_name) in name
+        ]
 
-    # ex5
     def _edit_name(self, name):
         return (
             name.lower()
@@ -23,38 +24,9 @@ class YouTubeLessonManager:
         )
 
 
-# Tests
-# ex1
-lesson_manager = YouTubeLessonManager()
-print(lesson_manager.lessons)
-
-# ex2
 lesson_manager = YouTubeLessonManager()
 lesson_manager.save("For-Loops", "https://www.youtube.com/watch?v=OnDr4J2UXSA")
-print(
-    lesson_manager.lessons
-)  # outputs: {"For-Loops": "https://www.youtube.com/watch?v=OnDr4J2UXSA"}
-# print(
-#     lesson_manager.lessons["For-Loops"]
-# )  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
-
-# ex3
+print(lesson_manager._lessons["forloops"])  # outputs: OnDr4J2UXSA
 print(
     lesson_manager.get("For-Loops")
-)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
-
-# ex4
-print(
-    lesson_manager.get("for-loops")
-)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
-print(
-    lesson_manager.get("fOr-LooPS")
-)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
-
-# ex5
-print(
-    lesson_manager.get("for-loops")
-)  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
-print(
-    lesson_manager.get("for loops")
 )  # outputs: 'https://www.youtube.com/watch?v=OnDr4J2UXSA'
