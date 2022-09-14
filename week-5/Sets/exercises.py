3d8ba61b0c471b36b4536b811e5b8c4a3d6dcc07
tom-nachum <tom.nachum@gmail.com>
2022-09-14 17:52:44 +0300

    finished ex1&2

--- week-5/Sets/exercises.py
+++ week-5/Sets/exercises.py
@@ -13,3 +13,21 @@ def find_jobs_by_word(word):
 
 
 # Junior Jobs in Brooklin
+entry_level_agencys = set(
+    [job["agency"] for job in nyc_jobs if job["career_level"] == "Entry-Level"]
+)
+broadway_agencys = set(
+    [job["agency"] for job in nyc_jobs if "Broadway" in job["work_location"]]
+)
+relevant_agencys = entry_level_agencys & broadway_agencys
+print(relevant_agencys)
+
+# Max Hourly Salary
+not_entry_level_salarys = set(
+    [job["salary_range_to"] for job in nyc_jobs if job["career_level"] != "Entry-Level"]
+)
+hourly_salarys = set(
+    [job["salary_range_to"] for job in nyc_jobs if job["salary_frequency"] == "Hourly"]
+)
+max_salary = max(not_entry_level_salarys & hourly_salarys)
+print(max_salary)
