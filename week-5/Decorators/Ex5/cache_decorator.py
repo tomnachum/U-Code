2eafbec6165af1cdc1d070adc4c484a024b58cab
tom-nachum <tom.nachum@gmail.com>
2022-09-15 16:18:48 +0300

    finished bonus of cache decorator

--- week-5/Decorators/Ex5/cache_decorator.py
+++ week-5/Decorators/Ex5/cache_decorator.py
@@ -1,15 +1,28 @@
 from functools import wraps
+import inspect
+
+
+def get_default_args(func):
+    signature = inspect.signature(func)
+    return {
+        k: v.default
+        for k, v in signature.parameters.items()
+        if v.default is not inspect.Parameter.empty
+    }
+
+
+def parse_arguments(args, kwargs, default_args):
+    return tuple(args) + tuple(sorted({**default_args, **kwargs}.items()))
 
 
 def cache(func):
     previous_calls = {}
+    default_args = get_default_args(func)
 
     @wraps(func)
     def inner(*args, **kwargs):
-        nonlocal previous_calls
-        all_args = tuple(args) + tuple(kwargs.items())
-        print(args, kwargs)
-        print(all_args)
+        nonlocal previous_calls, default_args
+        all_args = parse_arguments(args, kwargs, default_args)
         if all_args in previous_calls:
             return previous_calls[all_args]
         result = func(*args, **kwargs)
--- week-5/Decorators/Ex5/main.py
+++ week-5/Decorators/Ex5/main.py
@@ -11,3 +11,21 @@ def fibonacci(n):
 
 print(fibonacci(2))
 print(fibonacci(4))
+
+
+@cache
+# Note: the * make the kwargs "mandatory kwargs", which mean we must pass them as a key value pair.
+# https://www.youtube.com/watch?v=R8-oAqCgHag&ab_channel=mCoding
+def add(x, *, addition=0, more=0):
+    print("running  add")
+    return x + 1 + addition + more
+
+
+add(1, addition=0, more=0)
+add(1, more=0, addition=0)
+add(1, more=0)
+add(1)
+add(2)
+add(1, addition=5)
+add(1, addition=5, more=2)
+add(1, addition=5, more=2)
