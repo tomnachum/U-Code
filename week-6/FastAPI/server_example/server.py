6c97d94dfc9bbfa9b45590e5e6ff2b3d8d4e6db7
tom-nachum <tom.nachum@gmail.com>
2022-09-18 11:31:56 +0300

    added query params to server

---
+++ week-6/FastAPI/server_example/server.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Someone has made a request and accessed the server. Exciting!"}
+
+
+@app.get("/maps")
+def maps():
+    return "Here's some stuff related to maps"
+
+
+@app.get("/shoobi")
+def shoobi():
+    return "This here is the shoobi *route*"
+
+
+@app.get("/landing/{username}")
+async def greet_user(username):
+    return {"message": "Hi there {}".format(username)}
+
+
+@app.get("/optionalParameters/")
+async def query_params(name):
+    return {"message": "Hi there {}".format(name)}
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
