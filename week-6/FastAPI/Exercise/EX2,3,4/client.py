68fe0db47fa24a8c71ec0609a2af021489ff7088
tom-nachum <tom.nachum@gmail.com>
2022-09-18 15:19:13 +0300

    finished ex5

--- week-6/FastAPI/Exercise/EX2,3,4/client.py
+++ week-6/FastAPI/Exercise/EX2,3,4/client.py
@@ -6,5 +6,20 @@ def get_price(name):
     print(res["price"])
 
 
-get_price("table")
-get_price("x")
+# get_price("table")
+# get_price("x")
+
+
+def buy_item():
+    name = input("Which item do you want to buy?")
+    res = requests.get(f"http://localhost:8000/buy/{name}").json()
+    item = res["item"]
+    if item is not None:
+        print(
+            f"Congratulations, you've just bought {item['name']} for {item['price']}. There are {item['inventory']} left now in the store."
+        )
+    else:
+        print("There is no such item in store")
+
+
+buy_item()
