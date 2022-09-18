c1fe16a19d5986789c8041e225a06b2833f2ecb9
tom-nachum <tom.nachum@gmail.com>
2022-09-18 15:08:29 +0300

    finished ex4

--- week-6/FastAPI/Exercise/EX2,3,4/server.py
+++ week-6/FastAPI/Exercise/EX2,3,4/server.py
@@ -13,5 +13,14 @@ def get_price(name):
     return {"price": None}
 
 
+@app.get("/buy/{name}")
+def buy_item(name):
+    for item in store:
+        if item["name"] == name:
+            item["inventory"] -= 1 if item["inventory"] > 0 else 0
+            return {"item": item}
+    return {"item": None}
+
+
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
