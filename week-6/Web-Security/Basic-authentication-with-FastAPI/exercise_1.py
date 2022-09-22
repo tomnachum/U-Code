975e4d0427221e6bab0a77ed552b053b56aa1df9
tom-nachum <tom.nachum@gmail.com>
2022-09-22 11:59:43 +0300

    finished ex2

--- week-6/Web-Security/Basic-authentication-with-FastAPI/exercise_1.py
+++ week-6/Web-Security/Basic-authentication-with-FastAPI/exercise_1.py
@@ -11,7 +11,8 @@ def verify_authorization_header(auth_header: str) -> Tuple[List[str], FastAPIUse
         first_name="Code", last_name="Specialist", user_id=1
     )  # Usually you would decode the JWT here and verify its signature to extract the 'sub'
     scopes = [
-        "admin"
+        "admin",
+        "standard",
     ]  # You could for instance use the scopes provided in the JWT or request them by looking up the scopes with the 'sub' somewhere
     return scopes, user
 
@@ -24,6 +25,12 @@ app.add_middleware(
 
 @app.get("/")  # Sample endpoint (secured)
 @requires("admin")
+def home(request: Request):
+    return request.user
+
+
+@app.get("/profile")
+@requires("standard")
 def home(request: Request):
     return request.user
 
