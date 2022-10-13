74e7d892e8f02fc1e7fbef507a929792429092b3
tom-nachum <tom.nachum@gmail.com>
2022-10-13 10:43:46 +0300

    finished setup and instructions of sql in python

---
+++ week-9/SQL-in-python/setup.sql
@@ -0,0 +1,14 @@
+USE jobs;
+-- CREATE DATABASE jobs;
+-- CREATE TABLE company(
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(20),
+--     industry VARCHAR(20),
+--     employees INT
+-- );
+-- INSERT INTO Company (name, industry, employees)
+-- VALUES ('Google', 'hi tech', 1000);
+-- INSERT INTO Company (name, industry, employees)
+-- VALUES ('CyberArk', 'hi tech', 2000);
+SELECT *
+FROM company
