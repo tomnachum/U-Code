101c2fa653092e551a8099bb1d7e5b8b3bc41fb6
tom-nachum <tom.nachum@gmail.com>
2022-11-01 14:01:28 +0200

    start working on zoo

---
+++ week-12/SOLID/zoo/constants.ts
@@ -0,0 +1,11 @@
+// daily food in kg
+const LION_MEAT = 6;
+const WHALE_PLANKTON = 2000;
+const CLOWN_FISH_ALGAE = 0.003;
+const GOOSE_SEEDS = 1;
+const OWL_WORMS = 0.1;
+
+// species
+const MAMMALS = "Mammals";
+const FISH = "Fish";
+const BIRDS = "Birds";
