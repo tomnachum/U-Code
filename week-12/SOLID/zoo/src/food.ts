9c7b0102b6e7bb7e8a5644b8b4ed38ac9170f191
tom-nachum <tom.nachum@gmail.com>
2022-11-03 14:14:00 +0200

    react intro commit

--- week-12/SOLID/zoo/src/food.ts
+++ week-12/SOLID/zoo/src/food.ts
@@ -1,8 +1,5 @@
 class Food {
   constructor(private _type: string, private _dailyQuantityG: number) {}
-  toString(): string {
-    return `Food:\n \t Type: ${this._type} \n \t Daily G: ${this._dailyQuantityG}`;
-  }
 
   public get type(): string {
     return this._type;
@@ -10,5 +7,9 @@ class Food {
 
   public get dailyQuantityKG(): number {
     return this._dailyQuantityG;
+  }
+
+  toString(): string {
+    return `Food:\n \t Type: ${this._type} \n \t Daily G: ${this._dailyQuantityG}`;
   }
 }
