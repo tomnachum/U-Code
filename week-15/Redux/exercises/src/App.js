cb8325ebea19a75acb6aee339ee0a9cfbf90e3a7
tom-nachum <tom.nachum@gmail.com>
2022-11-23 13:58:28 +0200

    finished ex 3+4

--- week-15/Redux/exercises/src/App.js
+++ week-15/Redux/exercises/src/App.js
@@ -4,11 +4,19 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { RandomCounter } from "./features/randomCounter/RandomCounter";
 import { useSelector, useDispatch } from "react-redux";
-import { increment as randomCounterIncrement } from "./features/randomCounter/randomCounterSlice";
-import { increment as counterIncrement } from "./features/counter/counterSlice";
+import {
+  increment as randomCounterIncrement,
+  selectRandomCount,
+} from "./features/randomCounter/randomCounterSlice";
+import {
+  increment as counterIncrement,
+  selectCount,
+} from "./features/counter/counterSlice";
 
 function App() {
   const dispatch = useDispatch();
+  const count = useSelector(selectCount);
+  const randomCount = useSelector(selectRandomCount);
 
   return (
     <div className="App">
@@ -25,6 +33,8 @@ function App() {
         >
           Increase both counters
         </button>
+
+        <label htmlFor="">{Math.max(count, randomCount).toFixed(2)}</label>
       </header>
     </div>
   );
